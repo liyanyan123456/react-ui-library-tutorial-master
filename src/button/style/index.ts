53c51c6097513133b4f57729f1df986b442b23f8
liyanyan123456 <2073820553@qq.com>
2025-08-25 18:23:22 +0800

    fix: 修复组件

--- src/button/style/index.ts
+++ src/button/style/index.ts
@@ -2,7 +2,7 @@
  * @Author: liyanyan123456 2073820553@qq.com
  * @Date: 2025-08-25 18:09:26
  * @LastEditors: liyanyan123456 2073820553@qq.com
- * @LastEditTime: 2025-08-25 18:09:36
+ * @LastEditTime: 2025-08-25 18:23:36
  * @FilePath: \react-ui-library-tutorial-master\src\button\style\index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */

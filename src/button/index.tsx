2b546ef9608ad8294eca4c5ada554b4967495b03
liyanyan123456 <2073820553@qq.com>
2025-08-25 19:22:15 +0800

    feat: 增加按钮样式

--- src/button/index.tsx
+++ src/button/index.tsx
@@ -2,7 +2,7 @@
  * @Author: liyanyan123456 2073820553@qq.com
  * @Date: 2025-08-25 16:59:16
  * @LastEditors: liyanyan123456 2073820553@qq.com
- * @LastEditTime: 2025-08-25 17:55:30
+ * @LastEditTime: 2025-08-25 19:01:55
  * @FilePath: \react-ui-library-tutorial-master\src\button\button.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -20,7 +20,7 @@ const defaultProps = {
   text: 'button',
 };
 
-const prefixCls = 'button';
+const prefixCls = 'c-button';
 
 const Button: React.FC<ButtonProps> = (userProps) => {
   const props = { ...defaultProps, ...userProps };
